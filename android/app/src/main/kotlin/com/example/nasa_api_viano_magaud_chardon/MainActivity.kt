2f47633991e31b5e9013a7df1c9d235413ffa542
Kespion <kesputecryo@gmail.com>
2025-06-24 09:54:50 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/nasa_api_viano_magaud_chardon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nasa_api_viano_magaud_chardon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:nasa_api_viano_magaud_chardon/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
